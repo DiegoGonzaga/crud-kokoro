4235d037f88f53137880e9291ba572d065fe4d5f
Diego de Melo Gonzaga <diego.gonzaga.prestador@alleasy.com.br>
2022-01-13 16:11:16 -0300

    Now is possible create a user and log in with it

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -4,11 +4,17 @@
       <div class="column is-4 is-offset-4">
         <h1 class="title">Log in</h1>
 
-        <form>
+        <form @submit.prevent="submitForm">
           <div class="field">
             <label for="email">Email</label>
             <div class="control">
-              <input id="email" type="email" name="email" class="input" />
+              <input
+                id="email"
+                type="email"
+                name="email"
+                class="input"
+                v-model="username"
+              />
             </div>
           </div>
 
@@ -20,8 +26,15 @@
                 name="password"
                 class="input"
                 id="password"
+                v-model="password"
               />
             </div>
+          </div>
+
+          <div class="notification is-danger" v-if="errors.length">
+            <ul>
+              <li v-for="error in errors" v-bind:key="error">{{ error }}</li>
+            </ul>
           </div>
 
           <div class="field">
@@ -36,7 +49,48 @@
 </template>
 
 <script>
+import axios from "axios";
+import store from "../store";
+import router from "../router";
 export default {
   name: "Login",
+  data() {
+    return { username: "", password: "", errors: [] };
+  },
+  methods: {
+    submitForm() {
+      axios.defaults.headers.common["Authorization"] = "";
+      localStorage.removeItem("token");
+
+      const formData = {
+        username: this.username,
+        password: this.password,
+      };
+
+      axios
+        .post("/api/v1/token/login/", formData)
+        .then((response) => {
+          const token = response.data.auth_token;
+
+          store.commit("setToken", token);
+
+          axios.defaults.headers.common["Authorization"] = "Token " + token;
+
+          localStorage.setItem("token", token);
+
+          router.push("/dashboard/my-account");
+        })
+        .catch((error) => {
+          this.errors = [];
+          if (error.response) {
+            for (const property in error.response.data) {
+              this.errors.push(`${property}: ${error.response.data[property]}`);
+            }
+          } else if (error.message) {
+            this.errors.push("Something went wrong. Please try again!");
+          }
+        });
+    },
+  },
 };
 </script>
--- src/views/dashboard/MyAccount.vue
+++ src/views/dashboard/MyAccount.vue
@@ -5,11 +5,35 @@
         <h1 class="title">My Account</h1>
       </div>
     </div>
+
+    <div class="column is-12">
+      <button @click="logout()" class="button is-danger">Log out</button>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+import store from "../../store";
+import router from "../../router";
 export default {
   name: "MyAccount",
+  methods: {
+    async logout() {
+      await axios
+        .post("/api/v1/token/logout/")
+        .then((response) => {
+          console.log("Logged out");
+        })
+        .catch((error) => {
+          console.log(JSON.stringify(error));
+        });
+
+      axios.defaults.headers.common["Authentication"] = "";
+      localStorage.removeItem("token");
+      store.commit("removeToken");
+      router.push("/");
+    },
+  },
 };
 </script>
